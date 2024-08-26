381ffdc7e1dc12df3cca0de980945cfdc95b8f43
Leonardo Prasetyo <leonardo.prasetyo5@gmail.com>
2024-08-26 11:41:40 +1000

    added scsss,

--- app.vue
+++ app.vue
@@ -1,6 +1,11 @@
 <template>
-  <div>
-    <NuxtRouteAnnouncer />
-    <NuxtWelcome />
+  <div class="test">
+    Test wordd
   </div>
 </template>
+
+<style lang="scss" scoped>
+.test {
+  font-size: $test
+}
+</style>
