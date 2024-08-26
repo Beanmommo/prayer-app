64edf3abb13d4bcee9970343f1a25daee1dbc79d
Leonardo Prasetyo <leonardo.prasetyo5@gmail.com>
2024-08-26 11:46:37 +1000

    added google fonts

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -2,6 +2,17 @@
 export default defineNuxtConfig({
   compatibilityDate: "2024-04-03",
   devtools: { enabled: true },
+  modules: [
+    [
+      "@nuxtjs/google-fonts",
+      {
+        families: {
+          Inter: [400, 500, 600, 700, 800],
+        },
+        download: true,
+      },
+    ],
+  ],
   css: ["@/assets/scss/main.scss"],
   vite: {
     css: {
@@ -12,4 +23,6 @@ export default defineNuxtConfig({
       },
     },
   },
+
+  modules: ["@nuxtjs/google-fonts"],
 });
